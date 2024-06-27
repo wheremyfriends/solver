e2d93e238d5fe5cef42b7048511a39c53a34583f
WhyAre <laiyr52@gmail.com>
2024-06-27 22:02:44 +0800

    Add more logging to run.ts

--- src/run.ts
+++ src/run.ts
@@ -2,9 +2,10 @@ import { getOptimisedTimetable } from "./index";
 import { timetable } from "../timetables/input";
 import { Roarr as log } from "roarr";
 
-const NUMSOLS = 1;
+const NUMSOLS = -1;
 const INDEX = 0;
 const TIMETABLES = [timetable];
 
 const res = getOptimisedTimetable(TIMETABLES, INDEX, NUMSOLS);
 log(Object(res), "res");
+log(`Length: ${res.length}`);
